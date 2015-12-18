ebf3076d27ed9024d6ee49f8cc1eca8225b51996
王其 <wangqi504635@gmail.com>
2015-12-18 10:15:26 +0800

    no message

--- JZMbyWriter.py
+++ JZMbyWriter.py
@@ -4,13 +4,7 @@ from commen import *
 from time import sleep
 from bs4 import *
 import re
-
-base_url = 'http://www.juzimi.com/writer/%E5%BC%A0%E7%88%B1%E7%8E%B2'
-page_num = 66
-cur_page = 0
-url = base_url
-data_re = '\'text\':\'(.*)\',\'desc\''
-filename = "writer.txt"
+from urllib.parse import quote
 
 
 def write_jz(html):
@@ -25,9 +19,34 @@ def write_jz(html):
                         f.write('\n\n')
 
 
-while (cur_page <= page_num):
-    html = getHtml(url)
-    write_jz(html)
-    cur_page += 1
-    url = base_url + "?page=" + str(cur_page)
-    sleep(3)
+def get_all_html(total, base_url):
+    cur_page = 0
+    url = base_url
+    while (cur_page <= total):
+        sleep(3)
+        html = getHtml_proxy(url, proxy_ip)
+        write_jz(html)
+        cur_page += 1
+        url = base_url + "?page=" + str(cur_page)
+
+
+def get_page_num(url):
+    span_re = '.*<span class="xqreplynumhidmax">(.*)</span>跳到'
+    html = getHtml_proxy(url, proxy_ip)
+    span = re.findall(span_re, html)
+    if len(span) == 0:
+        return 0
+    return int(span.pop())
+
+
+pre_url = 'http://www.juzimi.com/writer/'
+proxy_ip = '120.195.207.99'
+page_num = 0
+data_re = '\'text\':\'(.*)\',\'desc\''
+filename = "writer.txt"
+writes = ['戴望舒']
+for item in writes:
+    filename = item + '.txt'
+    base_url = pre_url + quote(item)
+    page_num = get_page_num(base_url)
+    get_all_html(page_num, base_url)
